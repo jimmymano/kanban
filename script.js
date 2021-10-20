3c00a0ad38cb1997e0785069e07a6db76f5745f2
Jimmy Jam <jimmymano@mail.com>
2021-10-19 19:07:44 -0500

    created add item functionality

--- script.js
+++ script.js
@@ -103,6 +103,31 @@ createItemEl(backlogList,0,backlogItems,index);
   updateSavedColumns();
 }
 
+// Add to column list,Reset text box
+function addToColumn(column){
+   const itemText = addItems[column].textContent;
+   const selectedArray = listArrays[column];
+   selectedArray.push(itemText);
+   addItems[column].textContent = '';
+   updateDOM();
+}
+
+
+// Show add item input box
+function showInputBox(column){
+addBtns[column].style.visibility = 'hidden';
+saveItemBtns[column].style.display = 'flex';
+addItemContainers[column].style.display = 'flex';
+}
+
+// Hide item input box
+function hideInputBox(column){
+    addBtns[column].style.visibility = 'visible';
+    saveItemBtns[column].style.display = 'none';
+    addItemContainers[column].style.display = 'none';
+    addToColumn(column);
+}
+
 // Allow arrays to reflect drag and drop items
 function rebuildArrays(){
 console.log(backlogList.children);
